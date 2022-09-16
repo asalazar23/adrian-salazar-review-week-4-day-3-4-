88b0f40f3e25a4e80a015cadec54d97d4689e7fc
Adrian Salazar <113551883+asalazar23@users.noreply.github.com>
2022-09-16 19:17:26 +0000

    learned .pop(), which removes the last item in a list

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 # remember to fork this repl into your own account
 # create a new github project for this. call it week 4 review : lists, strings manipulations, tuples, substrings
-print("adrian")
+#print("adrian")
 
 
 ##############################################################################################################
 # basic review
 # Find and display on the screen which character occupies the fifth position within the following word:
-"computer"
+# computer = "computer"
+# print(computer[-1])
 
 # Find and display the index of the last occurrence of the word "practice" in the following sentence:
-"In theory, theory and practice are the same. In practice, they are not."
+# phrase = "In theory, theory and practice are the same. In practice, they are not."
+# print(phrase.rindex("practice"))
 
 ###############################################Review############################################################
 #####################################challenge 1################################################################
 # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
+my_list = ("drums", "piano", "guitar", "bass", "ukelele")
+
+
 
 # Add the element "motorcycle" to the following list of means of transportation:
 
 transportation_means = ["plane", "car", "ship", "bicycle"]
-
+transportation_means.append("motorcycle")
+print(transportation_means)
+transportation_means.remove("car")
+print(transportation_means)
+transportation_means.pop()
+print(transportation_means)
+#.pop() removes the last item
+print("my new trans is " +newTrans)
 # You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
 
 # here are two lists:
